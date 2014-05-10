f8ceef9910b872b8d02897eab0a42576a1c1e29b
David C. Bishop <david@davidbishop.org>
2014-05-10 19:12:35 +1000

    Stores and retrieves aliases from JSON.

--- alias.py
+++ alias.py
@@ -1,15 +1,121 @@
+import json
+
+
+class Alias:
+    def __init__(self, alias, command, category=None):
+        self.alias = alias
+        self.command = command
+        self.category = category
+        if category == "":
+            self.category = None
+
+    def __eq__(self, other):
+        if other is None:
+            return -1
+        if not isinstance(other, Alias):
+            return -1
+        return ((self.alias == other.alias) and
+                (self.command == other.command) and
+                (self.category == other.category))
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __repr__(self):
+        return str((self.alias, self.command, self.category))
+
+
+def get_sh_alias_command(alias, command):
+    return 'alias %s="%s"\n' % (alias, command)
+
+
+def aliases_to_tuplelist(aliases):
+    """
+    Convert a dict containing aliases to a sorted list to tuples.
+    """
+    alias_list = []
+    for key, value in aliases.items():
+        alias_list.append((key, value.command))
+    return sorted(alias_list)
+
+
 class Aliases:
     def __init__(self, backend):
         self.backend = backend
 
-    def add_alias(self, alias, command):
-        self.backend.add_alias(alias, command)
+    def add_alias(self, alias):
+        self.backend.add_alias(alias)
 
-    def get_zsh_script(self):
+    def get_sh_script(self):
         script = ""
         aliases = self.backend.get_aliases()
 
-        for alias in aliases:
-            script = script + 'alias ' + alias[0] + '="' + alias[1] + '"\n'
+        alias_list = aliases_to_tuplelist(aliases)
+
+        for alias in alias_list:
+            new_line = get_sh_alias_command(alias[0], alias[1])
+            script = script + new_line
 
         return script
+
+
+class AliasesJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Alias):
+            dobj = {
+                'command': obj.command,
+                'category': obj.category
+            }
+            return dobj
+        return super(AliasesJSONEncoder, self).encode(obj)
+
+
+def dict_to_alias(alias, item):
+    """
+    Convert a dict to an Alias object.
+    """
+    return Alias(alias, item['command'], item['category'])
+
+
+def dicts_to_aliases(dlist):
+    """
+    Convert a dict of dicts to a dict of Alias objects.
+    """
+    olist = {}
+    for item in dlist:
+        alias = dict_to_alias(item, dlist[item])
+        olist[item] = alias
+    return olist
+
+
+class AliasesJSONDecoder(json.JSONDecoder):
+    def decode(self, json_string):
+        if json_string == '':
+            raise "WTF"
+        default_obj = super(AliasesJSONDecoder, self).decode(json_string)
+        if 'aliases' in default_obj:
+            alias_objects = dicts_to_aliases(default_obj['aliases'])
+            default_obj['aliases'] = alias_objects
+        return default_obj
+
+
+class JSONBackend:
+    def __init__(self, fp):
+        self.fp = fp
+
+    def get_aliases(self):
+        self.fp.seek(0)
+        d = json.load(self.fp, cls=AliasesJSONDecoder)
+        if 'aliases' in d:
+            return d['aliases']
+        return {}
+
+    def write_aliases(self, aliases):
+        self.fp.seek(0)
+        json.dump({'aliases': aliases}, self.fp, cls=AliasesJSONEncoder,
+                  indent=4, sort_keys=True)
+
+    def add_alias(self, alias):
+        aliases = self.get_aliases()
+        aliases[alias.alias] = alias
+        self.write_aliases(aliases)
--- test_alias.py
+++ test_alias.py
@@ -1,24 +1,100 @@
 import unittest
-from alias import Aliases
+import json
+import io
+import alias
+from alias import Alias, Aliases, JSONBackend
 
 
 class FakeAliasDatabase():
     def __init__(self):
         self.aliases = []
 
-    def add_alias(self, alias, command):
-        self.aliases.append((alias, command))
+    def add_alias(self, alias):
+        self.aliases.append(alias)
 
     def get_aliases(self):
         return self.aliases
 
 
+def make_fake_aliases():
+    return {'lst': Alias("lst", "ls -lhar --sort time"),
+            'lss': Alias("lss", "ls -lhar --sort time")}
+
+
+SIMPLE_ALIAS_JSON = """
+{
+    "aliases": {
+        "lst" :{
+            "command": "ls -lhar --sort time",
+            "category": null
+        }
+    }
+}
+"""
+
+
+def get_simple_alias_json_stringio():
+    return io.StringIO(SIMPLE_ALIAS_JSON)
+
+
+class TestAliasObj(unittest.TestCase):
+    def test_comparisons(self):
+        a1 = Alias('lst', 'ls -lhar --sort time')
+        a2 = Alias('lst', 'ls -lhar --sort time')
+        a3 = Alias('lss', 'ls -lhar --sort size')
+
+        self.assertEqual(a1, a2)
+        self.assertNotEqual(a1, a3)
+
+
+class TestJSON(unittest.TestCase):
+    def test_dicts_to_aliases(self):
+        example = {
+            "lst": {
+                'command': 'ls -lhar --sort time',
+                'category': None
+            },
+            "lss": {
+                'command': 'ls -lhar --sort size',
+                'category': None
+            }
+        }
+
+        expected = {
+            'lst': Alias('lst', 'ls -lhar --sort time'),
+            'lss': Alias('lss', 'ls -lhar --sort size')
+        }
+
+        result = alias.dicts_to_aliases(example)
+
+        self.assertDictEqual(result, expected)
+
+    def test_decode(self):
+        f = get_simple_alias_json_stringio()
+        json.load(f, cls=alias.AliasesJSONDecoder)
+
+
 class TestAlias(unittest.TestCase):
+    def test_parse_alias(self):
+        f = get_simple_alias_json_stringio()
+        backend = JSONBackend(f)
+        aliases = Aliases(backend)
+        script = aliases.get_sh_script()
+        self.assertEqual(script, 'alias lst="ls -lhar --sort time"\n')
+
     def test_add_alias(self):
-        alias_db = FakeAliasDatabase()
-        alias = Aliases(alias_db)
+        f = io.StringIO('{"aliases": {}}')
+        backend = JSONBackend(f)
+        alias = Aliases(backend)
 
-        alias.add_alias("lst", "ls --lhar --sort time")
-        script = alias.get_zsh_script()
+        alias.add_alias(Alias("lst", "ls -lhar --sort time"))
+        alias.add_alias(Alias("lss", "ls -lhar --sort size"))
+        script = alias.get_sh_script()
 
-        self.assertEqual('alias lst="ls --lhar --sort time"\n', script)
+        self.assertEqual('alias lss="ls -lhar --sort size"\n' +
+                         'alias lst="ls -lhar --sort time"\n', script)
+
+    def test_backend(self):
+        f = io.StringIO()
+        backend = JSONBackend(f)
+        backend.write_aliases(make_fake_aliases())
